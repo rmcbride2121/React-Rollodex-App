d39e4abb149757c1a51a0cd3e3b58821e8fde695
rmcbride2121 <rmcbride2121@gmail.com>
2019-06-08 15:15:14 -0500

    finished rolodex project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
+function UserCard(props) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={props.picture} className="img-cirlce" />
+      <p>{props.firstName}</p>
+      <span>{props.lastName}</span>
+      <button className="btn btn-info" onClick={props.onClick}>Hide Details</button>
+      <p>{props.email}</p>
     </div>
   );
+}
+
+function HiddenCard(props) {
+
+  return (
+    <div>
+      <img src={props.picture} className="img-cirlce" />
+      <p>{props.firstName}<span>{props.lastName}</span></p>
+      <button className="btn btn-info" onClick={props.onClick}>Show Details</button>
+    </div>
+  );
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      isHidden: true
+    };
+  }
+
+  onClick = (event, props) => {
+    this.setState(prevState => ({
+      isHidden: !prevState.isHidden
+    }));
+
+    if (this.state.isHidden === false) {
+      return (
+        <p>email</p>
+      )
+    }
+  };
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?results=25")
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        console.log(json);
+        this.setState({
+          results: json.results
+        });
+      });
+  }
+
+  render() {
+    if (this.state.isHidden === false){
+      return (
+        <div className="App">
+          <p>This is working</p>
+          {this.state.results.map((results, index) => (
+            <UserCard
+              key={index}
+              firstName={results.name.first}
+              lastName={results.name.last}
+              email={results.email}
+              picture={results.picture.medium}
+              onClick={this.onClick}
+            />
+          ))}
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <p>This is working</p>
+          {this.state.results.map((results, index) => (
+            <HiddenCard
+              key={index}
+              firstName={results.name.first}
+              lastName={results.name.last}
+              picture={results.picture.medium}
+              onClick={this.onClick}
+            />
+          ))}
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
